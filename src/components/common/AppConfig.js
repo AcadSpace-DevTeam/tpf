ff8998c06d55725c9e49a54f32a027fd2737c62a
zapdal <dev.arunavo@acadspace.org>
2024-12-10 20:22:00 +0530

    first commit

---
+++ src/components/common/AppConfig.js
@@ -0,0 +1,4 @@
+export const BASE_URL='https://b2blogin-437904.el.r.appspot.com/api';
+// edit
+// export const BASE_URL='https://testenv.acadspace.org/api';
+// export const BASE_URL='http://localhost:8080/api';
